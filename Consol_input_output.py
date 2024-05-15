04a49a2541419dc218fe8cc3ceadaafac12c2bfa
Shubham Krishna shinde <118288116+shubhamSS9@users.noreply.github.com>
2024-05-15 06:30:46 +0530

    Add files via upload

---
+++ Consol_input_output.py
@@ -0,0 +1,51 @@
+# fname, mname, lname = input("Enter Full Name: ").split()
+# print(fname, mname, lname)
+
+# data = input("Enter name, age, salary: ").split()
+# name = data[0]
+# age = int(data[1])
+# salary = int(data[2])
+
+# print(name, age, salary)
+
+# name, age, salary = "shubham", 26, 45000
+# print(f"name={name} age={age} salary={salary}")
+
+# name = "shubham krishna shinde"
+# for i in name.split():
+#     print(f"{i[:10]}")
+ 
+
+# name, age, salary = "shubham", 26, 45000
+
+# print("name: {} \nage: {} \nsalary: {}".format(name, age, salary))
+
+# print("age: {2} \nsalary: {2} \nname: {1}".format(name, age, salary))
+
+# print("name={1:1} salary={1:0}".format(name, salary))
+
+# print("salary={0:10.3f}".format(salary))
+
+
+
+
+# r, l, b = input("Enter the parameter: ").split()
+
+# radius = int(r)
+
+# length = int(l)
+
+# breadth = int(b)
+
+# circumference_of_circle = 2 * 3.14 * radius
+
+# perimeter_of_rectangle = 2 * (length + breadth)
+
+# print(circumference_of_circle, "circumference of circle")
+
+# print(perimeter_of_rectangle, "Perimeter of rectangle")
+
+# l, b = 1.555665, 10.5
+# length = float(l)
+# breadth= float(b)
+# print("length={} breadth={}".format(length, breadth))
